412fc0a212ce770aedb9b3d887dd466e39506b8d
AEGS10 <alvarogarcia400ag@gmail.com>
2025-11-30 23:10:24 -0500

    Agregar boton para cambios dinamicos

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function saludar() {
     alert("Hola desde feature-mensaje");
 }
+
+function cambiarMensaje() {
+    const mensaje = document.getElementById("mensaje");
+    mensaje.textContent = "¡El mensaje cambió desde commit de práctica!";
+}
